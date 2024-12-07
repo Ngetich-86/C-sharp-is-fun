471b7213346454f1a2d1b6516d11b1b4af19f20f
Ngetich-86 <gideonkngetich86@gmail.com>
2024-12-07 12:12:24 +0300

    added grading system

---
+++ demotest/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+//grading system
+
+using System;
+namespace grading{
+    class Program{
+        static void Main(string[] args)
+        {
+            int grade;
+            Console.Write("Enter your marks: ");
+            grade = Convert.ToInt32(Console.ReadLine()); //converts the string to integer
+
+            if(grade <= 100 && grade >= 70)
+            {
+                Console.WriteLine("A");
+            }
+            else if(grade < 70 && grade >= 60) {
+                Console.WriteLine("B");
+            }
+            else if(grade < 60 && grade >= 50) {
+                Console.WriteLine("C");
+            }
+            else if(grade < 50 && grade >= 40) {
+                Console.WriteLine("D");
+            }
+            else
+            {
+                Console.WriteLine("you failed!!!");
+            }
+        }
+    }
+}
